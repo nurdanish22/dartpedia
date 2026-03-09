02f81cbd76015c1871d60445ebc274ee3e32e913
Trigger <nurdanish22@gmail.com>
2026-03-10 00:27:58 +0800

    Initial commit

---
+++ cli/bin/cli.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:command_runner/command_runner.dart';
+const version = '0.0.1';
+
+void main(List<String> arguments) async { // main is now async and awaits the runner
+var runner = CommandRunner(); // Create an instance of the new CommandRunner
+await runner.run(arguments); // Call its run method, awaiting its Future<void>
+}
